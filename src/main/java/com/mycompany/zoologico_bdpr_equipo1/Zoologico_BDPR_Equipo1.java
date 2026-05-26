93e6e3208a625bdeb95ac2789c351f91b0f8eb43
DAL225 <255dal@gmail.com>
2026-05-26 09:06:32 -0600

    Commit 1

---
+++ src/main/java/com/mycompany/zoologico_bdpr_equipo1/Zoologico_BDPR_Equipo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zoologico_bdpr_equipo1;
+
+/**
+ *
+ * @author aburt
+ */
+public class Zoologico_BDPR_Equipo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
